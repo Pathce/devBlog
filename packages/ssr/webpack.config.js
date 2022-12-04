41145554dd91586ef1fd9a30714edf1d7ec4f8c1
Pathce <asfgzx@naver.com>
2022-12-04 21:52:26 +0900

    UPDATE: SSR 준비

--- packages/ssr/webpack.config.js
+++ packages/ssr/webpack.config.js
@@ -1,32 +1,37 @@
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+// const webpack = require('webpack');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  entry: './src/index.tsx',
-  devServer: {
-    historyApiFallback: true,
-    port: 3000,
-    hot: true
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.json'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(t|j)sx?$/,
-        use: ['babel-loader', 'ts-loader']
-      },
-    ],
-  },
+// module.exports = {
+//   entry: './src/index.tsx',
+//   devServer: {
+//     historyApiFallback: true,
+//     port: 3000,
+//     hot: true
+//   },
+//   resolve: {
+//     extensions: ['.ts', '.tsx', '.js', '.json'],
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.(t|j)sx?$/,
+//         use: ['babel-loader', 'ts-loader']
+//       },
+//     ],
+//   },
 
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: './public/index.html',
-    }),
-  ],
-  mode: 'development'
-  // mode: 'production'
+//   plugins: [
+//     new webpack.HotModuleReplacementPlugin(),
+//     new HtmlWebpackPlugin({
+//       filename: 'index.html',
+//       template: './public/index.html',
+//     }),
+//   ],
+//   mode: 'development'
+//   // mode: 'production'
+// }
+
+
+module.exports = function (env) {
+  return require(`./webpack.${env}.js`)
 }
