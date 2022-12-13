f242aee01c9f3b4bebf8473a1a10a946bc7615af
Pathce <asfgzx@naver.com>
2022-12-13 22:02:11 +0900

    UPDATE: ui 디렉토리에 로그인화면 만들 준비

--- packages/ui/src/App.tsx
+++ packages/ui/src/App.tsx
@@ -1,24 +1,9 @@
-import React, { useState } from 'react';
-import { ThemeProvider } from 'styled-components';
-import { Button, TestServerComponent } from 'components';
-import { StyledLayout, StyledButton, lightTheme, darkTheme } from 'styled';
+
 
 function App() {
-  const [theme, setTheme] = useState(lightTheme);
-
-  const toggleTheme = () => {
-    setTheme(theme.value === 'light' ? darkTheme : lightTheme);
-  };
 
   return (
     <div className="App">
-      <ThemeProvider theme={theme}>
-        <StyledLayout>
-          <StyledButton variant={'primary'} onClick={toggleTheme}>Toggle Theme</StyledButton>
-          <Button />
-          <TestServerComponent />
-        </StyledLayout>
-      </ThemeProvider>
     </div>
   );
 }
